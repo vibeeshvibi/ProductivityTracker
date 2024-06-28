90cc44b98be8757128a794b89338650f62266904
vibeeshvibi <vibeeshnataraj1@gmail.com>
2024-06-28 17:03:56 +0530

    Initial Commit

---
+++ frontend/src/components/Tasks.jsx
@@ -0,0 +1,14 @@
+import Task from "./Task";
+
+const Tasks = ({ tasks, deleteTask }) => {
+  return (
+    <>
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} deleteTask={deleteTask} />
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
+
